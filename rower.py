ee00952f8c4d690625f10bf9c0bc2c5f7d360723
soramenium <37555197+soramenium@users.noreply.github.com>
2024-05-15 21:25:48 +0200

    Add files via upload

--- rower.py
+++ rower.py
@@ -14,14 +14,16 @@ def play_random_sound(folder_path):
 
 def main():
     folder_path = "./sounds"  # Change this to your sound folder path
-    print("Press 'p' to play a random sound. Press 'q' to quit.")
-    while True:
-        key_pressed = keyboard.read_event(suppress=True)
-        if key_pressed.name == "p":
-            play_random_sound(folder_path)
-        elif key_pressed.name == "q":
-            print("Exiting...")
-            break
+    # print("Press 'p' to play a random sound. Press 'q' to quit.")
+    # while True:
+    #     key_pressed = keyboard.read_event(suppress=True)
+    #     if key_pressed.name == "p":
+    #         play_random_sound(folder_path)
+    #     elif key_pressed.name == "q":
+    #         print("Exiting...")
+    #         break
+    for i in 10:
+        play_random_sound(folder_path)
 
 if __name__ == "__main__":
     main()
